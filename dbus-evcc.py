9a7c0c2a3f1c67869404320ea1fd11a0a7f52600
cm <cm@coretec.at>
2025-04-28 11:21:17 +0200

    chargeCurrent is now offeredCurrent, see evcc issue #18537

--- dbus-evcc.py
+++ dbus-evcc.py
@@ -143,9 +143,9 @@ class DbusEvccChargerService:
             self._dbusservice['/Ac/Voltage'] = voltage
 
             self._dbusservice['/Ac/Power'] = float(loadpoint['chargePower']) # w
-            self._dbusservice['/Current'] = float(loadpoint['chargeCurrent'])
+            self._dbusservice['/Current'] = float(loadpoint['offeredCurrent'])
 
-            self._dbusservice['/SetCurrent'] = float(loadpoint['chargeCurrent'])
+            self._dbusservice['/SetCurrent'] = float(loadpoint['offeredCurrent'])
             self._dbusservice['/MaxCurrent'] = int(loadpoint['maxCurrent']) # int(data['ama'])
 
 
